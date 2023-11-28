7e1d52d137ad228678e19d469c322406e665e368
Sergio Belena <Sergio.X.BeleAaBoix@QuestDiagnostics.com>
2023-11-28 20:26:14 +0100

    Get users data - Fix globals.css generated outside folder app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
-export default function Home() {
-  
+export default async function Home() {
+  const data = await fetch("https://jsonplaceholder.typicode.com/users/");
+  const users = await data.json();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
+      {JSON.stringify(users)}
     </main>
-  )
+  );
 }
